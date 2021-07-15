2e833697a5a1a96865e996cb2171f9a32ebe73dc
kmrshubham123 <kmrshubham123@gmail.com>
2021-07-15 22:41:18 +0530

    [Kumar Shubham]Added:Create a Address Book Data Table with Different attributes

--- AddressBookLINQ/AddressBookLINQ/AddressBookService.cs
+++ AddressBookLINQ/AddressBookLINQ/AddressBookService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Text;
 
 namespace AddressBookLINQ
@@ -9,6 +10,21 @@ namespace AddressBookLINQ
     /// </summary>
     public class AddressBookService
     {
-        
+        DataTable table = new DataTable("AddressBook");
+        /// <summary>
+        /// UC2:-Create a address Book Data Table with different Attributes
+        /// </summary>
+        public void AddressBook()
+        {
+           
+            table.Columns.Add("FirstName");
+            table.Columns.Add("LastName");
+            table.Columns.Add("Address");
+            table.Columns.Add("City");
+            table.Columns.Add("State");
+            table.Columns.Add("Zip");
+            table.Columns.Add("PhoneNumber");
+            table.Columns.Add("Email");
+        }
     }
 }
